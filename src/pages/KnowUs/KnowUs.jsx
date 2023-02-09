8314c86222b1aac3b26f099a3945799a72311ecf
marumosquera <mosqueramariaelisa1998@gmail.com>
2023-02-09 19:13:11 -0300

    mobile ui emails hidden

--- src/pages/KnowUs/KnowUs.jsx
+++ src/pages/KnowUs/KnowUs.jsx
@@ -10,19 +10,19 @@ export const KnowUs = () => {
         <img src="./assets/diego.jpeg" alt="" className='img-us' />
         <p>Diego Cazap</p>
         <p className='text-slate-700'>Co-founder</p>
-        <p className='text-slate-400'>diego.cazap@piltri.io</p>
+        <p className='text-slate-400 mail'>diego.cazap@piltri.io</p>
       </div>
       <div className='w-1/3'>
         <img src="./assets/maru.jpeg" alt="" className='img-us'/>
         <p>Elisa Mosquera</p>
         <p className='text-slate-700'>Co-founder</p>
-        <p className='text-slate-400'>elisa.mosquera@piltri.io</p>
+        <p className='text-slate-400 mail'>elisa.mosquera@piltri.io</p>
       </div>
       <div className='w-1/3'>
         <img src="./assets/juli.jpeg" alt="" className='img-us'/>
         <p>Julian Lavagna</p>
         <p className='text-slate-700'>Co-founder</p>
-        <p className='text-slate-400'>julian.lavagna@piltri.io</p>
+        <p className='text-slate-400 mail'>julian.lavagna@piltri.io</p>
       </div>
       </div>
       

7b85b56ef27729d1bac82ee0333b391bb61a7f3f
marumosquera <mosqueramariaelisa1998@gmail.com>
2023-02-09 18:34:10 -0300

    our mission text

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './HomeBanner.css';
+import React from "react";
+import "./HomeBanner.css";
 
 export const HomeBanner = () => {
   return (
-    <div className='title-container'>
-        <span className='home-title'>
-
-          WEB3 DONATION PLATFORM FOR <br/> <span className='pr-4 pl-4 earth-title'> EARTH REGENERATION</span> <br/> PROJECTS </span>
+    <div className="title-container">
+      <span className="home-title">
+        WEB3 DONATION PLATFORM FOR 
+        <br />
+        <span className="pr-4 pl-4 earth-title"> EARTH REGENERATION</span>
+        <br />
+         PROJECTS{" "}
+      </span>
     </div>
-  )
-}
+  );
+};
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -4,36 +4,38 @@ import "./AboutPage.css";
 export const AboutPage = () => {
   return (
     <>
-    <div className="m-10">
-    <h1 className="title-about m-10">Our mission</h1>
-    <div className="flex about-container">
-  
-      <div className="about-page">
-        <p className="about-sentence mt-5">
-          We all want to change the world, but in order to make real changes we
-          need to help the projects that are working on making our world a
-          better place.
-        </p>
-        <p className="about-sentence mt-5">
-          There are 40B dollars locked in DeFi and a bunch of Regenerative
-          projects awaiting to be funded
-        </p>
-        <p className="about-sentence mt-5">
-          Our mission is to make donation simpler and attractive to you, that is
-          why we came up with an innovative solution that is a WIN-WIN.
-        </p>
-        <p className="about-sentence mt-5">
-          You want to do it, the money is there, but we are lacking incentives
-          to action
-        </p>
-        <p className="about-last mt-12">
-          If you donate through us, you get the chance to win money by
-          participating in a lotto!{" "}
-        </p>
+      <div className="m-10">
+        <h1 className="title-about m-10">Our mission</h1>
+        <div className="flex about-container">
+          <div className="about-page">
+            <p className="about-sentence mt-5">
+              We believe that the regenerative sector holds enormous potential
+              for creating a sustainable and equitable future, but that it often
+              struggles to secure the necessary funding.
+            </p>
+            <p className="about-sentence mt-5">
+              So, in order to generate a real impact, we need to help the
+              projects that are working on making our world a better place.
+            </p>
+            <p className="about-sentence mt-5">
+              Our mission is to make donation simpler and attractive to you,
+              that is why we came up with an innovative solution that is a
+              WIN-WIN.
+            </p>
+            <p className="about-sentence mt-5">
+              There’s over $40B locked in DeFi and a bunch of Earth regeneration
+              projects waiting to be funded.
+            </p>
+            <p className="about-sentence mt-5">
+              Our mission is to make donations simple and fun for you, that’s
+              why we came up with an innovative solution that is a WIN-WIN:
+            </p>
+            <p className="about-last mt-12">
+              Donate through us and get awesome IRL & crypto rewards!
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-   
     </>
   );
 };
